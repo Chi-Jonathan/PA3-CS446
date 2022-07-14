b950f289552d1b5ce8a8d84a485571582fbb5b67
Chi-Jonathan <jchi@nevada.unr.edu>
2022-07-14 15:00:37 -0700

    finished

--- analyze_txt.sh
+++ analyze_txt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Jonathan Chi/Chi-Jonathan
+#This code is meant to get us familiar with bash scripting by: outputting each unique word with a word count in alphabetical order, get every word with the suffix and prefix th sorted and counted, output every unique sentence with an uppercase letter, and output the first 5 and last 7 lines of a text
+
 filename="$1"
 grep -o -E "\w+" "$filename" | sort | uniq -i -c
 
--- hello_world.sh
+++ hello_world.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Jonathan Chi/Chi-Jonathan
+#This code is meant to demonstrate the very basics of bash scripting by printing hello world 5 times with a count.
+
 for var in {1..5};
 do
   echo "$var. Hello World!"
